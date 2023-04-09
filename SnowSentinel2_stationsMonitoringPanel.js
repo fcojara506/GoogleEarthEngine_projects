56b8a8487daa1c8950cde9f56b8ce8e76fb7b3ea
Francisco Jara <fcojara506@gmail.com>
2023-04-09 22:34:38 +0200

    Spanish to English, simplified text. General map

--- SnowSentinel2_stationsMonitoringPanel.js
+++ SnowSentinel2_stationsMonitoringPanel.js
@@ -1,27 +1,40 @@
-Map.add(ui.Label("Cargando..."))
+Map.add(ui.Label("Loading..."));
 
-var clipmap = function(image){return image.clip(rectangle)};
-var rectangle = ee.Geometry.Rectangle([-76, -18, -67, -55]);
+// Define region
+var Region2 = ee.FeatureCollection('users/franciscojara/MAULE/estaciones_maule_2021');
+
+var Region_buffer100m = createBuffer(Region2, 100);
+var Region_buffer1000m = createBuffer(Region2, 1000).geometry().dissolve();
 
 var Start_period = ee.Date('2020-01-01');
-var End_period   = ee.Date(Date.now());
-var initial_value= End_period.advance(-4,'days')
+var End_period = ee.Date(Date.now());
+var initial_value = ee.Date('2020-06-26');
 
-var sentinel2 = ee.ImageCollection('COPERNICUS/S2_SR')
-                  .filterDate(initial_value, End_period)
-                  .select('MSK_SNWPRB')
-                  .map(function(img){return img.updateMask(img.gt(0))})
-                  .mean()
+var sentinel2 = getImageCollection(initial_value, 3, Region_buffer1000m);
 
 // Name of the legend for legend in UI Panel
 var names_legend = ['(0 , 25]','(25 - 50]','(50 - 75]','(75 - 100]'];
-//var palette =['ade8f4','00b4d8','0077b6','001d3d']; 
 var palette =['001d3d','0077b6','00b4d8','ade8f4']; 
 
-var Region1 = ee.FeatureCollection('users/franciscojara/MAULE/estaciones_maule_2021_buffer100m');
-var Region2     = ee.FeatureCollection('users/franciscojara/MAULE/estaciones_maule_2021');
-//var map_Region2 = ui.Map.Layer(Region2,{color: 'red'},'Sur',true,1);
 var names       = Region2.aggregate_array("Nombre");
+
+// FUNCTIONS
+function createBuffer(featureCollection, distance) {
+  return featureCollection.map(function (feature) {
+    return feature.buffer(distance);
+  });
+}
+
+
+function getImageCollection(initialDate, advanceDays, bufferedRegion) {
+  return ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+    .select('MSK_SNWPRB')
+    .filterBounds(bufferedRegion)
+    .filterDate(initialDate, initialDate.advance(advanceDays, 'days'))
+    .filterMetadata('CLOUDY_PIXEL_PERCENTAGE', 'less_than', 50) // Filter based on cloud cover percentage
+    .map(function (img) { return img.updateMask(img.gt(0)); })
+    .mean();
+}
 
 function removeLayer(name,map) {
   var layers = map.layers();
@@ -42,43 +55,48 @@ function removeLayer(name,map) {
   }
 }
 
+function addImageToMaps(image, style, name, maps) {
+  if (maps.length === 0) {
+    for (var i = 0; i < 7; i++) {
+      var name_station = names.get(i);
+      var coords = Region2.filter(ee.Filter.eq('Nombre', name_station)).first();
+      var roi = coords.geometry().buffer(1000); // Buffer of 1 km around the coords
+      
+      var map = ui.Map();
+      map.setOptions('TERRAIN');
+      var label_name = ui.Label('Loading...');
+      label_names.push(label_name);
+      map.add(label_name); // add label to each map
+      map.setControlVisibility(false); // show map layer ticks
+      
+      // Clip the image to the region of interest (ROI) and add it to the map
+      map.addLayer(image.clip(roi), style, name);
+      
+      var xMarker = coords.geometry().buffer(1); // Create a closer buffer around the point coords
+      map.addLayer(xMarker, {color: 'red'}, 'X Marker');
+      map.addLayer(Region_buffer100m, {color: 'green', pointShape: 'diamond', pointSize: 100}, 'Puntos', true, 1); // add points
+      map.centerObject(coords, 14); // center to the point
+      map.setLocked(false, 13, 21);
+      maps.push(map); // save each map
+    }
+  } else {
+    for (var j = 0; j < 7; j++) {
+      map = maps[j];
+      name_station = names.get(j);
+      coords = Region2.filter(ee.Filter.eq('Nombre', name_station)).first();
+      roi = coords.geometry().buffer(1000); // Buffer of 1 km around the coords
 
-function addImageToMaps(image,style,name,maps){
-
-if (maps.length===0){
-for (var i = 0; i < 7; i++) {
-  var name_station = names.get(i);
-  var coords       = Region2.filter(ee.Filter.eq('Nombre', name_station)).first();
-  var map = ui.Map();
-  map.setOptions('HYBRID');
-  var label_name=ui.Label('Cargando...');
-  label_names.push(label_name)
-  map.add(label_name); // add label to each map
-  //map.addLayer(sentinel2,{min: 0,max: 100.0,palette: ['fcbf49','98c1d9', '118ab2','d00000'],},'a'); // add a map
-  map.setControlVisibility(false); // show map layer ticks
-  map.addLayer(image,style,name);
-  map.addLayer(Region1,{color: 'green',pointShape: 'diamond', pointSize: 100},'Puntos',true,1); // add points
-  map.centerObject(coords,15); // center to the point
-  map.setLocked(false,13,24);
-  maps.push(map); // save each map
+      map.addLayer(image.clip(roi), style, name);
+      xMarker = coords.geometry().buffer(1); // Create a closer buffer around the point coords
+      map.addLayer(xMarker, {color: 'red'}, 'X Marker');
+      //map.addLayer(image.clip(roi).resample('bicubic'), style, name); // Add resampled image
+      
+      
+      map.addLayer(Region_buffer100m, {color: 'green', pointShape: 'diamond', pointSize: 100}, 'Puntos', true, 1); // add points
+    }
+  }
+  return maps;
 }
-}else{
-for (var j = 0; j < 7; j++) {
-  map=maps[j];
-  removeLayer('Puntos',map)
-  removeLayer(name,map)
-  map.addLayer(image,style,name);
-  map.addLayer(Region1,{color: 'green',pointShape: 'diamond', pointSize: 100},'Puntos',true,1); // add points
-}
-}
-return maps;
-}
-
-
-var label_names=[];
-var maps=addImageToMaps(sentinel2,{min: 0,max: 100.0,palette: palette,},"Sentinel 2",[]);
-var new_labels=Region2.map(get_snow_value(sentinel2)).aggregate_array("new_label");
-label_names.map(change_label)
 
  function get_snow_value(dataset) {
     // cast Feature
@@ -86,8 +104,10 @@ label_names.map(change_label)
     var station_name = ee.String(f.get('Nombre'))
     var newf = ee.Feature(f)
     var value = dataset.reduceRegion(ee.Reducer.first(), f.geometry(), 100).get('MSK_SNWPRB')
-    var value2=ee.Algorithms.If(value,value,"NA")
+    var roundedValue = ee.Number(value).format('%.1f');
     
+    var value2 = ee.Algorithms.If(value, roundedValue, "NA")
+
     var new_labeled= station_name.cat(' (').cat(value2).cat("%)")
     var val=ee.Feature(newf.set('new_label',new_labeled))
     return val
@@ -102,36 +122,34 @@ label_names.map(change_label)
     new_label.evaluate(function(newlabel){label_name.setValue(newlabel)} );
   }
 
-
-
 function renderDateRange(dateRange) {
   //print(dateRange)
-  var dataset_sentinel2 = ee.ImageCollection('COPERNICUS/S2_SR')
+  var dataset_sentinel2 = ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+                  .filterBounds(Region_buffer1000m) // Filter by buffered region
                   .filterDate(dateRange.start(),dateRange.end())
                   .select('MSK_SNWPRB')
                   .map(function(img){return img.updateMask(img.gt(0))})
                   .mean()
+  print(dateRange)
                   
-  /*
-  var dataset_sentinel3 = ee.ImageCollection('COPERNICUS/S3/OLCI')
-                  //.filterBounds(subset)
-                  .filterDate(dateRange.start(),dateRange.end())
-                  .select(['Oa08_radiance', 'Oa06_radiance', 'Oa04_radiance'])
-                  .mean()
-                  .multiply(ee.Image([0.00876539, 0.0123538, 0.0115198]))
- */
  // MAKE PANELS
- 
   new_labels=Region2.map(get_snow_value(dataset_sentinel2)).aggregate_array("new_label");
   addImageToMaps(dataset_sentinel2,{min: 0,max: 100.0,palette: palette,},"Sentinel 2",maps);
   label_names.map(change_label)
-  //ee.List.sequence(0, 7).map(change_label)
 }
+
+
+var label_names=[];
+var maps=addImageToMaps(sentinel2,{min: 0,max: 100.0,palette: palette,},"Sentinel 2",[]);
+var new_labels = Region2.map(get_snow_value(sentinel2)).aggregate_array("new_label");
+label_names.map(change_label)
+
+
 
 var slider = ui.DateSlider({
   start: Start_period,
   end: End_period,
-  period: 3, // Every 5 days
+  period: 3, // Every n days
   value: initial_value,
   onChange: renderDateRange,
   style: {
@@ -139,19 +157,21 @@ var slider = ui.DateSlider({
   whiteSpace:'nowrap',
   stretch: 'horizontal',
   position:'top-right',
-  margin : '10px'
+  margin : '10px',
+  backgroundColor: 'rgba(255, 255, 255, 0)',
+  color:'red'
   },
   });
   
 
 
 // Set position of panel
-var legend = ui.Panel({style: {position: 'top-left',
+var legend = ui.Panel({style: {position: 'bottom-left',
 padding: '8px 15px',
 backgroundColor: 'rgba(255, 255, 255, 0)',}});
 // Create legend title
 var legendTitle = ui.Label({
-  value: 'Probabilidad de nieve \n (%)',
+  value: 'Snow presence probability \n (%)',
   style: {
     fontWeight: 'bold',
     fontSize: '14px',
@@ -161,6 +181,7 @@ var legendTitle = ui.Label({
     }
 });
 legend.add(legendTitle);
+
 var makeRow = function(color, name) {
       // Create the label that is actually the colored box.
       var colorBox = ui.Label({style: {backgroundColor: '#' + color,
@@ -179,30 +200,50 @@ for (var i = 0; i < names_legend.length; i++) {
   }
   // Add the title to the panel
 
+var rectangle_outbounds = ee.Feature(Region_buffer1000m.bounds());
+var additionalMap = ui.Map();
+additionalMap.setOptions('TERRAIN');
+additionalMap.addLayer(rectangle_outbounds, {color: 'red'}, 'outbounds');
+additionalMap.centerObject(rectangle_outbounds, 3);
+additionalMap.setControlVisibility(false);
+additionalMap.setLocked(true, 3, 4);
+
 maps[3].add(legend);
 //maps[3].setControlVisibility(true);
-
 var mapGrid = ui.Panel(
     [
-      ui.Panel([slider,maps[3]], null, {stretch: 'both'}),
+      ui.Panel([slider, maps[3]], null, {stretch: 'both'}),
       ui.Panel([maps[0], maps[4]], null, {stretch: 'both'}),
       ui.Panel([maps[1], maps[5]], null, {stretch: 'both'}),
-      ui.Panel([maps[2], maps[6]], null, {stretch: 'both'})
-      ],
+      ui.Panel([maps[2], maps[6]], null, {stretch: 'both'}),
+      ui.Panel([additionalMap], null, {stretch: 'vertical'}),
+    ],
     ui.Panel.Layout.Flow('horizontal'), {stretch: 'both'});
-
+    
 ui.Map.Linker(maps, "change-zoom")
 
-var title = ui.Label('Probabilidad de presencia de nieve en estaciones meteorológicas Región del Maule',
+
+
+var title = ui.Label('Probability of snow presence at meteorological stations in the Maule Region, Chile',
 {stretch: 'horizontal', textAlign: 'center', fontWeight: 'normal', fontSize: '24px'});
 
-var subtitle1 = ui.Label({value:'Producto derivado de imágenes satelitales Sentinel 2 (resolución 10 m). La nubosidad puede afectar significativamente la confiabilidad.', style: {margin: '0px 0px 4px 10px'}});
-var subtitle2 = ui.Label({value:'La probabilidad asociada a cada estación aparece al costado del nombre. NA% indica ausencia o indetección.', style: {margin: '0 0 4px 10px'}});
-var subtitle3 = ui.Label({value:'Los rectángulos enmarcan una región de 0.001°(aprox. 100m) entorno a cada estación.', style: {margin: '0 0 4px 10px'}});
-var subtitle4 = ui.Label({value:'El menú de la izquierda permite especificar las fechas entre 01-01-2000 y el presente cercano.', style: {margin: '0 0 4px 10px'}});
-var subtitle5 = ui.Label({value:'La imagen de fondo corresponde al mapa base de Google Maps Satellite (Anacrónico).', style: {margin: '0px 0 0px 10px'}});
+var shortDescription = 'Product derived from Sentinel 2 satellite imagery (10 m resolution). Cloud cover might affects reliability of the product. The average probability of snow around 100m radius (green circles) is shown next to each station. Red circles around stations are 1m radius (zoom in!). Left menu allows date selection from 01-01-2020 to the near present (typically 3 days).';
 
-ui.root.widgets().reset([title,subtitle1,subtitle2,subtitle3,subtitle4,subtitle5, mapGrid]);
+var combinedSubtitle = ui.Label({
+  value: shortDescription,
+  style: {
+    margin: '0px 0px 4px 10px',
+    whiteSpace: 'normal'
+  }
+});
+
+var subtitle6 = ui.Label({
+  value: 'Created by Francisco Jara. University of Chile (2021). Source code available at: https://github.com/fcojara506/GoogleEarthEngine_projects ',
+  style: {
+    margin: '0px 0 0px 10px'
+  }
+});
+
+ui.root.widgets().reset([title, combinedSubtitle, mapGrid, subtitle6]);
 ui.root.setLayout(ui.Panel.Layout.Flow('vertical'));
-  
 
